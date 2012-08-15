09fa38724e8e05afc78f912da0ed78ef892da910
James Socol <james@mozilla.com>
2012-08-15 15:21:33 -0400

    Version bump for small bugfix.

--- adminplus/__init__.py
+++ adminplus/__init__.py
@@ -2,7 +2,7 @@ from django.contrib.admin.sites import AdminSite
 from django.utils.text import capfirst
 
 
-VERSION = (0, 1, 6)
+VERSION = (0, 1, 7)
 __version__ = '.'.join([str(x) for x in VERSION])
 
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-adminplus',
-    version='0.1.6',
+    version='0.1.7',
     description='Add new pages to the Django admin.',
     long_description=open('README.rst').read(),
     author='James Socol',
