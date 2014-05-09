bd8af7beabec955349ba2558a4e58626cf89ee86
James Socol <me@jamessocol.com>
2014-05-09 10:06:44 -0400

    Version v0.3

--- adminplus/__init__.py
+++ adminplus/__init__.py
@@ -2,5 +2,5 @@
 Django-AdminPlus module
 """
 
-VERSION = (0, 2, 1)
+VERSION = (0, 3)
 __version__ = '.'.join(map(str, VERSION))
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='Add new pages to the Django admin.',
     long_description=open('README.rst').read(),
     author='James Socol',
-    author_email='james@mozilla.com',
+    author_email='me@jamessocol.com',
     url='http://github.com/jsocol/django-adminplus',
     license='BSD',
     packages=find_packages(exclude=['test_settings', 'test_urlconf']),
@@ -24,6 +24,12 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
